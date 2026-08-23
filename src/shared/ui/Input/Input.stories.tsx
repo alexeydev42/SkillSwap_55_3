8efd41244e1eb370c49f402617a82617075d7db4
Albert Valeyev <albertthecreature@icloud.com>
2026-08-23 19:03:57 +0300

    feat: add Input component

---
+++ src/shared/ui/Input/Input.stories.tsx
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Input } from './Input'
+
+import iconEye from '../../assets/icons/icon-eye.svg'
+import iconEyeSlash from '../../assets/icons/icon-eye-slash.svg'
+
+const meta = {
+  title: 'Shared/UI/Input',
+  component: Input,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Input>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    label: 'Email',
+    placeholder: 'Введите email',
+    type: 'email',
+  },
+}
+
+export const Focus: Story = {
+  args: {
+    label: 'Email',
+    placeholder: 'Введите email',
+    type: 'email',
+  },
+}
+
+export const Error: Story = {
+  args: {
+    label: 'Пароль',
+    placeholder: 'Введите пароль',
+    type: 'password',
+    error: 'Неверный пароль',
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    label: 'Email',
+    placeholder: 'Введите email',
+    type: 'email',
+    disabled: true,
+  },
+}
+
+export const Password: Story = {
+  args: {
+    label: 'Пароль',
+    placeholder: 'Введите пароль',
+    type: 'text',
+    showPasswordIcon: <img src={iconEye} alt='' />,
+    hidePasswordIcon: <img src={iconEyeSlash} alt='' />,
+  },
+}
+
+export const PasswordHelperText: Story = {
+  args: {
+    label: 'Пароль',
+    placeholder: 'Введите пароль',
+    type: 'password',
+    helperText: 'Используйте минимум 8 символов',
+  },
+}
