96b5802fc48fd7996fb659f8b86a13884eeeb117
Egor Smirnov <smirnovegor0312@gmail.com>
2026-08-24 23:22:31 +0300

    feat: complete Textarea component styles and add Storybook stories

---
+++ src/shared/ui/Textarea/Textarea.stories.tsx
@@ -0,0 +1,37 @@
+import { Textarea } from './Textarea'
+import type { StoryFn } from '@storybook/react'
+
+export default {
+  title: 'Shared/UI/Textarea',
+  component: Textarea,
+  decorators: [
+    (Story: StoryFn) => (
+      <div style={{ maxWidth: '436px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export const Default = {
+  args: {
+    label: 'Описание',
+    placeholder: 'Коротко опишите, чему можете научить',
+  },
+}
+
+export const Error = {
+  args: {
+    label: 'Описание',
+    placeholder: 'Коротко опишите, чему можете научить',
+    error: 'Слишком короткое описание',
+  },
+}
+
+export const Disabled = {
+  args: {
+    label: 'Описание',
+    placeholder: 'Коротко опишите, чему можете научить',
+    disabled: true,
+  },
+}
