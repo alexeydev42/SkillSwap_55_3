9f71e0d8c20d925705fbaed9ba05efed163404c1
Albert Valeyev <albertthecreature@icloud.com>
2026-08-29 19:16:03 +0300

    fix: Добавлен пропс в компонент Modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,15 +1,19 @@
 import { ReactNode } from 'react'
 import styles from './Modal.module.css'
+import clsx from 'clsx'
 
 interface ModalProps {
   children: ReactNode
+  className?: string
 }
 
-export const Modal = ({ children }: ModalProps) => {
+export const Modal = ({ children, className }: ModalProps) => {
   return (
     <>
       <div className={styles.overlay}></div>
-      <div className={styles['modal-wrapper']}>{children}</div>
+      <div className={clsx(styles['modal-wrapper'], className ?? styles['default'])}>
+        {children}
+      </div>
     </>
   )
 }
