fbae2b2c75f3f95d222af26e1218edf4f642303c
Egor Smirnov <smirnovegor0312@gmail.com>
2026-08-30 17:06:16 +0300

    feat: add ProfileMenuDropdown widget (VERST-41)

---
+++ src/widgets/Header/ProfileMenuDropdown/ProfileMenuDropdown.stories.tsx
@@ -0,0 +1,10 @@
+import { ProfileMenuDropdown } from "./ProfileMenuDropdown";
+
+export default {
+  title: 'Header/ProfileMenuDropdown',
+  component: ProfileMenuDropdown,
+};
+
+export const Default = {
+  args: {}
+}
