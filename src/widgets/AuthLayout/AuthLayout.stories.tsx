db9593c5fed9c186f1e36a1dda79bf65d25ca04d
Yulia Deltsova <juliedeltsova@yandex.ru>
2026-08-31 23:24:53 +0300

    Import fix

--- src/widgets/AuthLayout/AuthLayout.stories.tsx
+++ src/widgets/AuthLayout/AuthLayout.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 import { AuthLayout } from './AuthLayout'
 import { AuthInfoBlockProps } from '../AuthInfoBlock'
 import { RegistrationProgress } from '../RegistrationProgress'
