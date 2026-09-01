4c79c947ccfa94194036d4b108315c5b79285526
Anastasia Koroleva <akoroleva_1992@mail.ru>
2026-09-01 21:27:20 +0700

    WIP: implement MyComponent

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,10 +1,26 @@
-// TODO: реализовать страницу LoginPage
+import {AuthHeader} from '../../widgets/AuthHeader'
+import {Button} from '../../shared/ui/Button'
+import styles from './LoginPage.module.css'
+
 
 export default function LoginPage() {
   return (
+  <AuthLayout>
+   <AuthHeader/>
     <main>
-      <h1>LoginPage</h1>
-      <p>Страница в разработке</p>
+      <Button>Выход</Button>
+      <div>
+        <Button>Продолжить с Google</Button>
+        <Button>Продолжить с Apple</Button>
+        <div className={styles.divider}><span>Или</span></div>
+        <form>
+          <label></label>
+          <input type="text"><input/>
+          </form>
+
+      </div>
+
     </main>
+    <AuthLayout/>
   )
 }
